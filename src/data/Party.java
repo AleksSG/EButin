6dcefb017ca800a469ad38dba5b2386dfb7472db
asg19 <samuilov.aleks@gmail.com>
2018-12-18 12:18:36 +0100

    party created & data

--- src/data/Party.java
+++ src/data/Party.java
@@ -1,5 +1,33 @@
 package data;
 
 final public class Party {
+    private final String name;
 
+    public Party(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Party party1 = (Party) o;
+        return name.equals(party1.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Party{" + "name='" + name + '\'' + '}';
+    }
 }
