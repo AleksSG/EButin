903a11e143e88b7ae80194b0343a0b50cf571de7
ivancolomer <31484161+ivancolomer@users.noreply.github.com>
2018-12-23 16:35:30 +0100

    Finished DigitalSignature implemmentation Added an utility class named DataTypeConverter, it has now one method used to convert an array of bytes to Hex-String.

--- src/data/DigitalSignature.java
+++ src/data/DigitalSignature.java
@@ -1,4 +1,38 @@
 package data;
 
+import utils.DataTypeConverter;
+
+import java.util.Arrays;
+
 public class DigitalSignature {
+
+    private final byte[] votingOption;
+
+    public DigitalSignature(byte[] votingOption) {
+        this.votingOption = votingOption;
+    }
+
+    public byte[] getVotingOption() {
+        return votingOption;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        DigitalSignature dataSignature1 = (DigitalSignature) o;
+        return Arrays.equals(votingOption, dataSignature1.votingOption);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(votingOption);
+    }
+
+    @Override
+    public String toString() {
+        return "DataSignature{" + "votingOption='" + DataTypeConverter.bytesToHex(votingOption) + '\'' + '}';
+    }
 }
--- src/data/MailAdress.java
+++ src/data/MailAdress.java
@@ -1,4 +1,5 @@
 package data;
 
 public class MailAdress {
+
 }
