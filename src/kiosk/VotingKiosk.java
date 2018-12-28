b85550b508ea61083871c189a50d4b6b2623192a
AleksSG <samuilov.aleks@gmail.com>
2018-12-28 18:36:18 +0200

    changes in VotingKiosk
    initialize VOteCounter and vote

--- src/kiosk/VotingKiosk.java
+++ src/kiosk/VotingKiosk.java
@@ -4,17 +4,26 @@ import data.MailAddress;
 import data.Nif;
 import data.Party;
 import exceptions.InvalidNifException;
+import exceptions.InvalidSetOfPartiesException;
+import exceptions.PartyNotFoundException;
 import services.*;
+
+import java.util.HashSet;
+import java.util.Set;
 
 
 public class VotingKiosk {
 
     private ElectoralOrganism elecOrg;
     private MailerService mService;
+    private Nif nif_votant_actual;
+    private VoteCounter voteCounter;
 
-    public VotingKiosk() {
+    public VotingKiosk() throws InvalidSetOfPartiesException {
         this.elecOrg = null;
         this.mService = null;
+        Set<Party> partySet = fillSet();
+        this.voteCounter = new VoteCounter(partySet);
     }
     public void setElectoralOrganism(ElectoralOrganism eO){
         this.elecOrg = eO;
@@ -22,14 +31,36 @@ public class VotingKiosk {
     public void setMailerService(MailerService mService){
         this.mService = mService;
     }
+    public void setNif(Nif nif) {
+        this.nif_votant_actual = nif;
+    }
     public void vote(Party party) throws InvalidNifException {
-        //Nif nif = getNifVotant();
-        Nif nif = new Nif("12345678A");
-        if (elecOrg.canVote(nif)) {
+        if (elecOrg.canVote(this.nif_votant_actual)) {
+            voteCounter.scrutinize(party);
+        }
 
+        if (quiereRecibo()) {
+            
         }
     }
     public void sendeReceipt(MailAddress address) {
 
+    }
+
+    private boolean quiereRecibo() {
+        return true;
+    }
+
+    public void introducirEmailReciboE(MailAddress email) {
+
+    }
+
+    private Set<Party> fillSet(){
+        Set<Party> partySet = new HashSet<>();
+        //get parties from database
+        partySet.add(new Party("PP"));
+        partySet.add(new Party("PSC"));
+        partySet.add(new Party("Cs"));
+        return partySet;
     }
 }
