39b1249e403ba7bbb37b979667ba22484bd0fae9
AleksSG <samuilov.aleks@gmail.com>
2019-01-01 01:09:56 +0200

    Happy New Year 2019!!!
    From Ivan and Aleks

---
+++ src/HappyNewYear.java
@@ -0,0 +1,11 @@
+public class HappyNewYear {
+    private final int year;
+
+    public HappyNewYear() {
+        this.year = 2019;
+    }
+
+    public int getTheNewYear() {
+        return year;
+    }
+}
