74914f8ee57aef1c9c7d1a3c70072fb4538e13f2
AleksSG <samuilov.aleks@gmail.com>
2019-01-04 21:00:55 +0100

    First setUp Main class
    VotingProcess

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,25 @@
+import kiosk.VotingKiosk;
+import verification.IdentityVerify;
+import verification.ManualVerification;
 
 public class Main {
+    private VotingKiosk votingKiosk;
+    private final IdentityVerify DEFAULT_VERIFICATION = new ManualVerification();
+    private IdentityVerify verification;
+
     public static void main(String[] args) {
         System.out.println("Hello World");
+        new Main().votingProcess();
+    }
+
+    public void votingProcess() {
+        if (demanarConsentiment()) {
+            //verification = new BiometricVerification();
+        }
+        verification = DEFAULT_VERIFICATION;
+    }
+
+    public boolean demanarConsentiment() {
+        return true;
     }
 }
